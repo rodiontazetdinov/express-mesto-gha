2f90808629c7d22623849a8d480682eb0a5023bb
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-11-10 14:47:24 +0300

    start: добавить зависимости и настроить точку входа

---
+++ app.js
@@ -0,0 +1,14 @@
+const { PORT = 3000 } = process.env;
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT);
