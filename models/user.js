568602aded847f3bab1f4feb5b9aac9dfcd25066
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2023-01-27 20:49:59 +0300

    feat: добавить схеме users функцию проверки пароля

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,5 +33,21 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne(({ email }))
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильный логин или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
